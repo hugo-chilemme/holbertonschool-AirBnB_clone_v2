ed267e54733eea7450929784e9a2c9249189507a
kiki3165 <kikikou82@gmail.com>
2023-03-21 14:24:38 +0000

    update user

--- models/user.py
+++ models/user.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column, String, ForeignKey, Integer, Float
 
 class User(BaseModel, Base):
         """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+    __tablename__ = "users"
     
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
