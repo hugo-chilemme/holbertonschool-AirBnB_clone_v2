df8d41db4d33d05b61dbaef7ff9308ebea42c5c8
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-21 13:42:17 +0000

    Trying to patch unknow database error

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,9 @@
 --set db and user
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db; 
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-ALTER USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'hbnb_dev'@'%';
+ALTER USER 'hbnb_dev'@'%' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'%';
 FLUSH PRIVILEGES;
 
