379489d8d47aa8e6e857ac28971a55b0c7b3bed7
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-03-23 10:28:27 +0000

    Patching task 6: error with relationship

--- models/review.py
+++ models/review.py
@@ -6,6 +6,7 @@ from models.base_model import BaseModel
 class Review(BaseModel):
     """ Review classto store review information """
     __tablename__ = "reviews"
+
     text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), nullable=False, ForeignKey('places.id'))
-    user_id = Column(String(60), nullable=False, ForeignKey('users.id'))
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
