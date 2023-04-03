627bf68eeeeffc96e3e94544c64671bfc84d151c
Hugo Chilemme <73786753+HugoCLI@users.noreply.github.com>
2023-04-03 19:34:45 +0200

    Adding another blank line between every function

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,9 +9,11 @@ app = Flask("hbnb_server")
 def hello_hbnb():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
