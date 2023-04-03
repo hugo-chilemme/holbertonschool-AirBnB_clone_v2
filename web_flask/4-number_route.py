b1909d079aeb07ef0fc7f056700be75d2ed9d3b7
Hugo Chilemme <73786753+HugoCLI@users.noreply.github.com>
2023-04-03 19:51:36 +0200

    Adding page /number/<n> to display <n> when he is a int

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Create server with default main page """
+from flask import Flask
+
+app = Flask("hbnb_server")
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_is(text):
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python_is(text="is cool"):
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    return str(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
