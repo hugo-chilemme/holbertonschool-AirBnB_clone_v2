a73724783d9b77b27251a4f23f3090b0a542bf22
Hugo Chilemme <73786753+HugoCLI@users.noreply.github.com>
2023-04-03 22:07:34 +0200

    Adding close function in a model engine

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -54,3 +54,6 @@ class DBStorage:
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
         self.__session = scoped_session(session_factory)
+
+    def close(self):
+        self.__session.remove()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -64,3 +64,8 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+        
+    def close(self):
+        """Reloading system"""
+        FileStorage.__objects = {}
+        self.reload()
