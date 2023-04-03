2c2ad3c08f545b8ac1d7f78d3f3ab419cba06385
Hugo Chilemme <73786753+HugoCLI@users.noreply.github.com>
2023-04-03 21:45:50 +0200

    Create a page to display a html file with the pass number

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,45 +1,48 @@
 #!/usr/bin/python3
 """ Create server with default main page """
+
+
 from flask import Flask
 from flask import render_template
+
 
 app = Flask("hbnb_server")
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ When the user access to the main page """
+    """When the user access to the main page"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def print_hbnb():
-    """ When the user access to the /hbnb """
+    """When the user access to the /hbnb"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def print_c_is(text):
-    """ When the user access to the /c/<text> """
+    """When the user access to the /c/<text>"""
     return "C " + text.replace('_', ' ')
 
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def print_python_is(text="is cool"):
-    """ When the user access to the /python/[<text>] """
+    """When the user access to the /python/[<text>]"""
     return "Python " + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def display_number(n):
-    """ When the user access to the /number/<int:n> """
+    """When the user access to the /number/<int:n>"""
     return str(n) + " is a number"
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def display_number(n):
-    """ When the user access to the /number_template/<int:n> """
+    """When the user access to the /number_template/<int:n>"""
     return render_template('5-number.html', n=n)
 
 
