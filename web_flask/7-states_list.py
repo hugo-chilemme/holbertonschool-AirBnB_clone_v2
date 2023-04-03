c6e13c99745159920c0f2e09459c701973ea0e7c
Hugo Chilemme <73786753+HugoCLI@users.noreply.github.com>
2023-04-03 23:15:54 +0200

    Adding page to list all state

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Create server with default main page """
+from flask import Flask, render_template
+from models import storage 
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """def index"""
+    return "Hello HBNB!"
+
+
+@app.teardown_appcontext
+def afterRequest(self):
+    storage.close()
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """def states_list"""
+    from models.state import State
+    states = []
+    for state in storage.all(State).values():
+        states.append(state.to_dict())
+    
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
