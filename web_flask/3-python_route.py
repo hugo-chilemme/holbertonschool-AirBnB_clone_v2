0c09b6166905ea73edfc24eaa7d12382e3e5700c
Hugo Chilemme <73786753+HugoCLI@users.noreply.github.com>
2023-04-03 19:44:50 +0200

    Adding page /python/<text> when display 'Python <text>' or if text is empty 'Python is cool'

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Create server with default main page """
+from flask import Flask
+
+app = Flask("hbnb_server")
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_is(text):
+    display_text = "C " + text.replace('_', ' ')
+    return display_text
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python_is(text = "is cool"):
+    display_text = "Python " + text.replace('_', ' ')
+    return display_text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
