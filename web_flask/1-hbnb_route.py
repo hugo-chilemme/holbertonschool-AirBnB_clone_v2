66a278956f00acc88f6b0a551889d0296b6e0bc9
Hugo Chilemme <73786753+HugoCLI@users.noreply.github.com>
2023-04-03 19:32:46 +0200

    Adding page /hbnb to display HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-""" Create server with default main """
+""" Create server with default main page """
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask("hbnb_server")
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
 
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    return 'HBNB'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
